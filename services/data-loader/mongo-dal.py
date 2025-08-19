edfbdc28cedcb827c8e7e1b813b2c0d90ddc6a0a
shuki <shukiheftler@gmail.com>
2025-08-19 18:00:22 +0300

    mongo conn + mongo ... yaml

---
+++ services/data-loader/mongo-dal.py
@@ -0,0 +1,37 @@
+import os
+from pymongo import MongoClient, errors
+
+class Connection:
+    def __init__(self):
+        self.user = os.getenv("MONGO_USER")
+        self.password = os.getenv("MONGO_PASSWORD")
+        self.host = os.getenv("MONGO_HOST", "localhost")
+        self.port = os.getenv("MONGO_PORT", "27017")
+        self.db = os.getenv("MONGO_DB")
+        self.auth = os.getenv("MONGO_AUTH_DB","admin")
+        self.client = None
+
+    def connect(self):
+        try:
+            self.client = MongoClient(
+                host=self.host,
+                port=int(self.port),
+                username=self.user,
+                password=self.password,
+                authSource=self.auth,
+                serverSelectionTimeoutMS=5000
+            )
+            self.client.server_info()
+            print("MongoDB connected successfully")
+            return self.client
+        except errors.ServerSelectionTimeoutError as e:
+            print("Failed to connect to MongoDB:", e)
+            return None
+        except errors.OperationFailure as e:
+            print("Authentication failed:", e)
+            return None
+
+    def close(self):
+        if self.client:
+            self.client.close()
+            print("MongoDB connection closed")
